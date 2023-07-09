f0bde9f61495b2392649b6bedb43c6d5b0552697
Sundram Yadav <hot2shortcut@gmail.com>
2023-07-10 02:11:25 +0530

    trying to add allure-report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from "cypress";
+// import allureWriter = require("@shelex/cypress-allure-plugin/writer");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
-    env: {
-      url: "https://rahulshettyacademy.com",
-      allureReuseAfterSpec: true,
-    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // const _ = require("lodash"); // yup, dev dependencies
-      // const path = require("path"); // yup, core node library
-      // const debug = require("debug"); // yup, dependencies
-      // const User = require("./lib/models/user"); // yup, relative local modules
       allureWriter(on, config);
       return config;
-
-      // console.log(__dirname); // /Users/janelane/Dev/my-project
-      // console.log(process.cwd()); // /Users/janelane/Dev/my-project
     },
     specPattern: ["**/*.cy.{js,jsx,ts,tsx}", "**/*.spec.{js,jsx,ts,tsx}"],
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
