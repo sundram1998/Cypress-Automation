1a0ccd69956f30b1e8a5a81a8118e6b6141ded20
Sundram Yadav <hot2shortcut@gmail.com>
2023-07-10 09:16:48 +0530

    Added Allure Report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,29 @@
 import { defineConfig } from "cypress";
 // import allureWriter = require("@shelex/cypress-allure-plugin/writer");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+var allure = require("allure-commandline");
+
+// returns ChildProcess instance
+var generation = allure(["generate", "allure-results"]);
+
+generation.on("exit", function (exitCode) {
+  console.log("Generation is finished with code:", exitCode);
+});
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
+    env: {
+      allureResultsPath: "allure-results",
+    },
     specPattern: ["**/*.cy.{js,jsx,ts,tsx}", "**/*.spec.{js,jsx,ts,tsx}"],
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
     excludeSpecPattern: [],
   },
   retries: { runMode: 1, openMode: 0 },
-  numTestsKeptInMemory: 50,
+  numTestsKeptInMemory: 0,
   defaultCommandTimeout: 25000,
   chromeWebSecurity: true,
 });
