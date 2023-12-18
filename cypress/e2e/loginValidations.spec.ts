836a1f1cbb8b58612fd14f3d60a73129312245fb
Sundram Yadav <hot2shortcut@gmail.com>
2023-12-18 18:13:18 +0530

    some changes

--- cypress/e2e/loginValidations.spec.ts
+++ cypress/e2e/loginValidations.spec.ts
@@ -1,10 +1,13 @@
+/// <reference types="cypress" />
+import "../support/commands";
 describe("validations reated to login", () => {
   it("Lgin to page with custom command", () => {
-    cy.visit("https://rahulshettyacademy.com/locatorspractice/");
-    const username = "Test.com";
-    const password = "1234";
-    cy.get("input[placeholder='Username']").type(username);
-    cy.get("input[placeholder='Password']").type(password);
+    cy.login("test", "password");
+    // cy.visit("https://rahulshettyacademy.com/locatorspractice/");
+    // const username = "Test.com";
+    // const password = "1234";
+    // cy.get("input[placeholder='Username']").type(username);
+    // cy.get("input[placeholder='Password']").type(password);
     cy.get("button[type='submit']").click();
 
     cy.get("li").filter(':contains("Services")').should("have.length", 2);
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,5 @@
 /// <reference types="cypress" />
 
-import cypress = require("cypress");
-
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -38,15 +36,27 @@ import cypress = require("cypress");
 //     }
 //   }
 // }
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>;
-      drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>;
-      dismiss(
-        subject: string,
-        options?: Partial<TypeOptions>
-      ): Chainable<Element>;
-    }
+// cypress/support/commands.js
+
+// Create a module or use an existing one (e.g., declare-cypress.d.ts)
+declare module "cypress" {
+  interface Chainable {
+    login(username: string, password: string): void;
   }
 }
+
+// Your Cypress command file (e.g., commands.js)
+declare namespace Cypress {
+  interface Chainable<Subject = any> {
+    // Use keyof to ensure that "login" is a valid key of Chainable<any>
+    login: (username: string, password: string) => Chainable<Subject>;
+  }
+}
+
+// Your Cypress command file (e.g., commands.js)
+Cypress.Commands.add("login", (username: string, password: string) => {
+  cy.visit("https://rahulshettyacademy.com/locatorspractice/");
+  cy.get("input#inputUsername").type(username);
+  cy.get('input[placeholder="Password"]').type(password);
+  cy.get("#login-button").click();
+});
